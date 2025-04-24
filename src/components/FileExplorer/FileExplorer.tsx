e396bb66e415471383f61af586b570af0f7d0ed1
Barry <barrymurphy_9@hotmail.com>
2025-04-24 19:08:56 +0100

    added open folder functionality

--- src/components/FileExplorer/FileExplorer.tsx
+++ src/components/FileExplorer/FileExplorer.tsx
@@ -2,19 +2,26 @@ import File from "../File/File";
 import logo from "./../../assets/icons8-folder.svg";
 import "./FileExplorer.css";
 import data from "../../data/data.json";
+import { useState } from "react";
 
 const FileExplorer = ({ items = data }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    !isOpen ? setIsOpen(true) : setIsOpen(false);
+  };
+
   return (
     <>
       {items.map((item, i) => {
         if (item.type === "folder") {
           return (
             <div key={i}>
-              <article className="folder">
+              <article onClick={handleClick} className="folder">
                 <img src={logo} />
                 <h3>{item.name}</h3>
               </article>
-              {item.files && (
+              {isOpen && item.files && (
                 <div>
                   <FileExplorer items={item.files} />
                 </div>

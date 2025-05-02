7d36a38ae7efd141c2c9b6f11e7dc971d09a1897
Barry <barrymurphy_9@hotmail.com>
2025-05-02 19:40:53 +0100

    removed type from Body component

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,20 +1,6 @@
 import FileExplorer from "../FileExplorer/FileExplorer";
 import items from "../../data/data";
 
-type FileItem = {
-  name: string;
-  type: string;
-  added: string;
-};
-
-type FolderItem = {
-  name: string;
-  type: "folder";
-  files: Item[];
-};
-
-type Item = FileItem | FolderItem;
-
 function Body() {
   return (
     <ul>
